dbf18bdbef8ca89d08ae52bbced60a802d7339dd
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-10-26 16:10:30 +0200

    merge ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { HeaderComponent } from './partials/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { MentionsComponent } from './components/mentions/mentions.component';
-
-
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [

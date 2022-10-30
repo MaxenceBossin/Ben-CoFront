c0759531585ca3826efee8c72c5becb6ebafbb4d
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-10-30 15:00:27 +0100

    navbar

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -23,15 +23,23 @@ export class MapComponent implements AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>'
     });
 
-    var greenIcon = L.icon({
-      iconUrl: './assets/images/marker.png',
-
-      iconSize: [43, 78], // size of the icon
-      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    var markerPlastique = L.icon({
+      iconUrl: './assets/images/marker_plastique.png',
+      iconSize: [30.75, 50.25], // size of the icon
+      iconAnchor: [10.75, 25.25], // point of the icon which will correspond to marker's location
+      popupAnchor: [5, -30] // point from which the popup should open relative to the iconAnchor
     });
-    L.marker([43.60899203730793, 1.4338861683142448], { icon: greenIcon }).addTo(this.map).bindPopup("Je suis une benne LaDen 😄 !!!");
+
+    var markerVerre = L.icon({
+      iconUrl: './assets/images/marker_verre.png',
+      iconSize: [30.75, 50.25], // size of the icon
+      iconAnchor: [10.75, 25.25], // point of the icon which will correspond to marker's location
+      popupAnchor: [5, -30] // point from which the popup should open relative to the iconAnchor
+    });
+    L.marker([43.60899203730793, 1.4338861683142448], { icon: markerVerre }).addTo(this.map).bindPopup("<h4 style='text-align:center;'>Benne à verre <br> Adresse: 20 rue de la liberté, Toulouse</h4> ");
+    L.marker([43.58427059082842, 1.4493199584962781], { icon: markerVerre }).addTo(this.map).bindPopup("Je suis une benne LaDen 😄 !!!");
+    L.marker([43.59819560098132, 1.4173909409965895], { icon: markerPlastique }).addTo(this.map).bindPopup("Je suis une benne LaDen 😄 !!!");
+    L.marker([43.57282977944497, 1.4476033446522087], { icon: markerPlastique }).addTo(this.map).bindPopup("Je suis une benne LaDen 😄 !!!");
 
     tiles.addTo(this.map);
 

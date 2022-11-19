13dc7f8798e87ddac3f79b679f27d6eb60108d2d
MaxenceBossin <maxbossin25@gmail.com>
2022-11-19 14:52:06 +0100

    register OK

--- src/app/components/auth/form-login/form-login.component.ts
+++ src/app/components/auth/form-login/form-login.component.ts
@@ -1,9 +1,7 @@
 import { I_LoginForm } from './../../../interfaces/login-form';
-import { I_JWTToken } from './../../../interfaces/jwttoken';
-import { HttpClient } from '@angular/common/http';
 import { AuthService } from './../../../service/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 
@@ -12,24 +10,19 @@ import {NgForm} from '@angular/forms';
   templateUrl: './form-login.component.html',
   styleUrls: ['../auth.component.css']
 })
-export class FormLoginComponent implements OnInit {
+export class FormLoginComponent{
 
   form: I_LoginForm = {
     email: '',
     password: ''
   }
 
-  constructor(private serviceAuth: AuthService, private http:HttpClient) { }
-
-  ngOnInit(): void {
-
-  }
+  constructor(private serviceAuth: AuthService) { }
 
   onSubmit(loginForm: NgForm) {
     this.form.email    = loginForm.value.email
     this.form.password = loginForm.value.password
 
-    console.log('h' , this.form);
     // Si la connexion est valide on stock le token en local storage +  TODO : rediriger vers la page d'accuiel
     this.serviceAuth.login(this.form).subscribe({
       next: (data) => this.serviceAuth.saveToken(data.token),
--- src/app/components/auth/form-register/form-register.component.ts
+++ src/app/components/auth/form-register/form-register.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from './../../../model/user';
+import { Component } from '@angular/core';
+import { AuthService } from './../../../service/auth/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['../auth.component.css']
 })
-export class FormRegisterComponent implements OnInit {
+export class FormRegisterComponent {
 
-  constructor() { }
+  user = new User()
 
-  ngOnInit(): void {
+  constructor(private serviceAuth: AuthService) { }
+
+
+
+
+  onSubmit(registerForm: NgForm) {
+    console.log(this.user);
+    console.log(registerForm.value);
+
+    this.user.email   = registerForm.value.email
+    this.user.password = registerForm.value.password
+    this.user.first_name = registerForm?.value.first_name
+    this.user.last_name = registerForm?.value.password
+
+    this.serviceAuth.register(this.user).subscribe({
+      next: (data) => console.log(data),
+      error: (e) => console.error(e),
+      complete: () => console.info('register success') 
+    })
+
+    
+
   }
 
 }

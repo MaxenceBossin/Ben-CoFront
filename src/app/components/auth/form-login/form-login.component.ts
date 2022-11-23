56688dad2673a356966afc6e0c799b79bf04a027
MaxenceBossin <maxbossin25@gmail.com>
2022-11-23 17:04:03 +0100

    redirection utilisateur en fonction de son role

--- src/app/components/auth/form-login/form-login.component.ts
+++ src/app/components/auth/form-login/form-login.component.ts
@@ -2,6 +2,8 @@ import { I_LoginForm } from './../../../interfaces/login-form';
 import { AuthService } from './../../../service/auth/auth.service';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import jwtDecode, { JwtPayload } from "jwt-decode";
+import {Router} from "@angular/router"
 
 
 
@@ -17,7 +19,9 @@ export class FormLoginComponent{
     password: ''
   }
 
-  constructor(private serviceAuth: AuthService) { }
+  jwtDecode?: any;
+
+  constructor(private serviceAuth: AuthService, private router: Router) { }
 
   onSubmit(loginForm: NgForm) {
     this.form.email    = loginForm.value.email
@@ -25,9 +29,25 @@ export class FormLoginComponent{
 
     // Si la connexion est valide on stock le token en local storage +  TODO : rediriger vers la page d'accuiel
     this.serviceAuth.login(this.form).subscribe({
-      next: (data) => this.serviceAuth.saveToken(data.token),
+      next: (data) => {
+        this.serviceAuth.saveToken(data.token),
+        this.jwtDecode =  jwtDecode<JwtPayload>(data.token);
+      },
       error: (e) => console.error(e),
-      complete: () => console.info('connection success') 
+      complete: () => {
+        console.info('connection success')
+        // redirection de l'utilisateur en fonction de son rôle
+        if(this.jwtDecode.roles.includes('ROLE_ADMIN')) {
+          return this.router.navigate(['/admin/planning'])
+        }
+        if(this.jwtDecode.roles.includes('ROLE_GARBAGE_COLLECTOR')) {
+          return this.router.navigate(['/']) // TODO
+        }
+        if(this.jwtDecode.roles.includes('ROLE_USER')) {
+          return this.router.navigate(['/']) // TODO 
+        }
+        return ""
+      }
     })
 
   }

f2c487a50bc03716b51ea486e470ac0ce9e5cd4f
AntoineBrevet <antoine.brevet@live.fr>
2022-11-30 18:38:31 +0100

    commit filter

--- src/app/partials/filters-bennes/filters-bennes.component.ts
+++ src/app/partials/filters-bennes/filters-bennes.component.ts
@@ -8,28 +8,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class FiltersBennesComponent implements OnInit {
   @Output() getFilterBenne = new EventEmitter<string>();
 
-  public filter = -1;
-
   constructor() { }
 
   ngOnInit(): void {
-    // console.log(this.mapComponent.lat);
-    // console.log(this.mapComponent.lon);
-    // console.log(this.mapComponent.map);
+
   }
 
   filterMap(param: any) {
     console.log('fix filter' , param);
     this.getFilterBenne.emit(param);
-  }
-
-  Filter(filter: any) {
-    // this.filter = filter;
-    // this.mapComponent.getFilter(this.filter);
-    // console.log(this.mapComponent.lat);
-    // console.log(this.mapComponent.lon);
-    // console.log(this.mapComponent.map);
-    // this.mapComponent.map.remove();
-    // this.mapComponent.initMap(this.mapComponent.lat, this.mapComponent.lon, filter);
   }
 }

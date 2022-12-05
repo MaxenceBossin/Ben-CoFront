53843f2027ec71b48696181edec7f20969164ff0
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-12-05 17:00:02 +0100

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://176.31.163.124:27931/api/',
-  api_url_login: 'http://176.31.163.124:27931/authentication_token',
+  api_url: 'https://176.31.163.124:27931/api/',
+  api_url_login: 'https://176.31.163.124:27931/authentication_token',
 };

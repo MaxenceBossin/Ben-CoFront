f82fdfc6e394daaeee99815ea10f40cfb50848c5
MaxenceBossin <maxbossin25@gmail.com>
2022-12-06 20:37:15 +0100

    liste eboueur

--- src/app/modules/admin/eboueur/eboueur.component.ts
+++ src/app/modules/admin/eboueur/eboueur.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { UsersService } from 'src/app/service/users/users.service';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user/user.service';
+import { User } from 'src/app/model/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-eboueur',
   templateUrl: './eboueur.component.html',
   styleUrls: ['./eboueur.component.css']
 })
-export class EboueurComponent implements OnInit {
-
-  constructor(private UsersService : UsersService) { }
+export class EboueurComponent implements OnInit, OnDestroy {
   listUsers : any
+  garbageCollectors?: any
+  subscribe : any
+
+  constructor(private UserService : UserService) { 
+  }
 
   ngOnInit() {
-    this.UsersService.getUsers().subscribe((data: any) => {
-      this.listUsers = data;
+    this.subscribe = this.UserService.getUsers().subscribe((users: any) => {
+      this.listUsers = users
+      this.garbageCollectors = users.filter((u:any) => {
+      return u.roles.includes('ROLE_GARBAGE_COLLECTOR')
+      })
     })
+    
+    
+  }
+
+  ngOnDestroy(): void {
+    this.subscribe?.unsubscribe()
   }
 }
